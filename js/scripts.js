735bd7cb5c52592efee59f8ea3f22182d792ecd8
Jaki Widby <jaki.widby@gmail.com>
2020-01-31 13:40:04 -0800

    remove value from var 'pizzaCost' withing 'pizzaPrice' prototype. Create var 'finalPizzaCost' to click event. CustomPizzaOrder values now passing through.

--- js/scripts.js
+++ js/scripts.js
@@ -5,35 +5,36 @@ function Pizza(size, meat,) {
 }
 
 Pizza.prototype.pizzaPrice = function() {
-  var pizzaCost = 0;
-  if (this.size === "small" && this.meat == 1) {
-    pizzaCost += 10;
-  } else if (this.size === "small" && this.meat == 2 || this.size === "medium" && this.meat == 1) {
-    pizzaCost += 12;
-  } else if (this.size === "medium" && this.meat == 2 || this.size === "large" && this.meat == 1) {
-    pizzaCost += 14;
-  } else if (this.size === "large" && this.meat == 2) {
-    pizzaCost += 16;
+  var pizzaCost;
+  if (this.size === "small" && this.meat === "1") {
+    pizzaCost = 10;
+  } else if (this.size === "small" && this.meat === "2" || this.size === "medium" && this.meat === "1") {
+    pizzaCost = 12;
+  } else if (this.size === "medium" && this.meat === "2" || this.size === "large" && this.meat === "1") {
+    pizzaCost = 14;
+  } else if (this.size === "large" && this.meat === "2") {
+    pizzaCost = 16;
   }
   // for (var i=0; i< this.toppings.length; i++) {
   //   pizzaCost += 1;
   // }
-  return(pizzaCost);
+  return pizzaCost;
 }
 
 $(document).ready(function() {
   $("button").click(function(event) {
     event.preventDefault();
-    var customPizzaOrder = new Pizza(customSize, customMeat);
     var customSize = $("input:radio[name=pizzaSize]:checked").val();
     var customMeat = $("input:radio[name=pizzaMeat]:checked").val();
+    var customPizzaOrder = new Pizza(customSize, customMeat);
+    var finalPizzaCost = customPizzaOrder.pizzaPrice();
     // var customToppings = [];
     // $("input:checkbox[name=pizzaToppings]:checked").each(function() {
     //   var checkedTopping = $(this).val();
     //   $(customToppings).push(checkedTopping);
     // });
-    customPizzaOrder.pizzaPrice();
-    console.log(Pizza.pizzaPrice);
+
+    console.log(finalPizzaCost);
     // console.log(customToppings);
     console.log(customSize);
     console.log(customMeat);
