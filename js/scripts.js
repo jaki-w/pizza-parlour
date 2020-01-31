2c6f05825fa7c200d8b2985c673988828870430d
Jaki Widby <jaki.widby@gmail.com>
2020-01-31 10:43:59 -0800

    change 'meats option' checkboxes to 'pizzaMeat' radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,22 @@ function Pizza(size, meat, toppings) {
   this.toppings = [];
 }
 
-Pizza.prototype.pizzaBaseCost = function(size, meat) {
+Pizza.prototype.pizzaBaseCost = function(this.size, this.meat) {
   var sizeCost = 0;
-  if (this.size === small && this.meat === 1) {
+  if (this.size === small && this.meat == 1) {
     sizeCost += 10;
-  } else if (this.size === small && this.meat === 2 || this.size === medium && this.meat === 1) {
+  } else if (this.size === small && this.meat == 2 || this.size === medium && this.meat == 1) {
     sizeCost += 12;
-  } else if (this.size === medium && this.meat === 2 || this.size === large && this.meat ===1) {
+  } else if (this.size === medium && this.meat == 2 || this.size === large && this.meat == 1) {
     sizeCost += 14;
   } else {
     sizeCost += 16;
   }
   return(sizeCost);
+}
+
+Pizza.prototype.toppingsCost = function(toppings) {
+  var totalToppings = 0;
+  for (var i = 0; i < toppings.length; i++)
+  return totalToppings + parseInt($(toppings[i]);
 }
